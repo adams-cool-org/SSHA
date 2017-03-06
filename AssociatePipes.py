a6cf7e2cc260b88d5e3bb41065dee734b6f15822
Christine Brown <christine.brown@phila.gov>
2017-03-06 15:20:35 -0500

    added warning for None type in uniqs

--- AssociatePipes.py
+++
@@ -1,9 +0,0 @@
-import arcpy
-from arcpy import env
-import HydraulicStudyGeneralTools
-
-#grab the project ID from the user
-project_id = arcpy.GetParameterAsText(0)
-
-#copy and asociate the wwGravMains to the study sewer laywer
-HydraulicStudyGeneralTools.associatePipes(project_id)
